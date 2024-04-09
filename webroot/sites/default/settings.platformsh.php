283afa3c701873ac59d4a009697d6f3cf3ef9f2c
Mads Nørgaard <madsnorgaard@live.dk>
2024-04-09 19:04:33 +0200

    OS2UOL-77 Add Platform.sh services config

--- webroot/sites/default/settings.platformsh.php
+++ webroot/sites/default/settings.platformsh.php
@@ -1,51 +1,166 @@
 <?php
-// Config sync directory
-// $settings['config_sync_directory'] = '../config/config_xxxxxxxxxx/sync';
+/**
+ * @file
+ * Platform.sh settings.
+ */
 
-// Database
-$databases['default']['default'] = array(
-  'database' => getenv('DB_NAME'),
-  'username' => getenv('DB_USER'),
-  'password' => getenv('DB_PASS'),
-  'prefix' => '',
-  'host' => getenv('DB_HOST'),
-  'port' => getenv('DB_PORT'),
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
+use Drupal\Core\Installer\InstallerKernel;
 
-// Trusted host pattern
-if (!empty(getenv('TRUSTED_HOSTS'))) {
-    $settings['trusted_host_patterns'] = explode(',', getenv('TRUSTED_HOSTS'));
+$platformsh = new \Platformsh\ConfigReader\Config();
+
+// Configure the database.
+if ($platformsh->hasRelationship('database')) {
+  $creds = $platformsh->credentials('database');
+  $databases['default']['default'] = [
+    'driver' => $creds['scheme'],
+    'database' => $creds['path'],
+    'username' => $creds['username'],
+    'password' => $creds['password'],
+    'host' => $creds['host'],
+    'port' => $creds['port'],
+    'pdo' => [PDO::MYSQL_ATTR_COMPRESS => !empty($creds['query']['compression'])],
+    'init_commands' => [
+      'isolation_level' => 'SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED',
+    ],
+  ];
 }
 
-if (!empty(getenv('REDIS_HOST')) && !empty(getenv('REDIS_PORT'))) {
-    // Redis configuration
-    $settings['redis.connection']['interface'] = 'PhpRedis';
-    // The host and port that the redis service is accessible on.
-    $settings['redis.connection']['host']      = getenv('REDIS_HOST');
-    $settings['redis.connection']['port']      = getenv('REDIS_PORT');
-
-    // Enable redis caching
-    // $settings['cache']['default'] = 'cache.backend.redis';
-    // $settings['cache']['bins']['render'] = 'cache.backend.redis';
-    // $settings['cache_prefix'] = 'stream';
-    // $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
+// Enable verbose error messages on development branches, but not on the production branch.
+// You may add more debug-centric settings here if desired to have them automatically enable
+// on development but not production.
+if (isset($platformsh->branch)) {
+  // Production type environment.
+  if ($platformsh->onProduction() || $platformsh->onDedicated()) {
+    $config['system.logging']['error_level'] = 'hide';
+  } // Development type environment.
+  else {
+    $config['system.logging']['error_level'] = 'verbose';
+  }
 }
 
-// Elasticsearch configuration
-if (!empty(getenv('ELASTIC_HOST')) && !empty(getenv('ELASTIC_PORT'))) {
-    $config['search_api.server.elasticsearch']['backend_config']['host'] = getenv('ELASTIC_HOST');
-    $config['search_api.server.elasticsearch']['backend_config']['port'] = getenv('ELASTIC_PORT');
-    $config['elasticsearch_connector.cluster.content']['url'] = 'http://' . getenv('ELASTIC_HOST') . ':' . getenv('ELASTIC_PORT');
+// Enable Redis caching.
+if ($platformsh->hasRelationship('redis') && !InstallerKernel::installationAttempted() && extension_loaded('redis') && class_exists('Drupal\redis\ClientFactory')) {
+  $redis = $platformsh->credentials('redis');
+
+  // Set Redis as the default backend for any cache bin not otherwise specified.
+  $settings['cache']['default'] = 'cache.backend.redis';
+  $settings['redis.connection']['host'] = $redis['host'];
+  $settings['redis.connection']['port'] = $redis['port'];
+
+  // Apply changes to the container configuration to better leverage Redis.
+  // This includes using Redis for the lock and flood control systems, as well
+  // as the cache tag checksum. Alternatively, copy the contents of that file
+  // to your project-specific services.yml file, modify as appropriate, and
+  // remove this line.
+  $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
+
+  // Allow the services to work before the Redis module itself is enabled.
+  $settings['container_yamls'][] = 'modules/contrib/redis/redis.services.yml';
+
+  // Manually add the classloader path, this is required for the container cache bin definition below
+  // and allows to use it without the redis module being enabled.
+  $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
+
+  // Use redis for container cache.
+  // The container cache is used to load the container definition itself, and
+  // thus any configuration stored in the container itself is not available
+  // yet. These lines force the container cache to use Redis rather than the
+  // default SQL cache.
+  $settings['bootstrap_container_definition'] = [
+    'parameters' => [],
+    'services' => [
+      'redis.factory' => [
+        'class' => 'Drupal\redis\ClientFactory',
+      ],
+      'cache.backend.redis' => [
+        'class' => 'Drupal\redis\Cache\CacheBackendFactory',
+        'arguments' => ['@redis.factory', '@cache_tags_provider.container', '@serialization.phpserialize'],
+      ],
+      'cache.container' => [
+        'class' => '\Drupal\redis\Cache\PhpRedis',
+        'factory' => ['@cache.backend.redis', 'get'],
+        'arguments' => ['container'],
+      ],
+      'cache_tags_provider.container' => [
+        'class' => 'Drupal\redis\Cache\RedisCacheTagsChecksum',
+        'arguments' => ['@redis.factory'],
+      ],
+      'serialization.phpserialize' => [
+        'class' => 'Drupal\Component\Serialization\PhpSerialize',
+      ],
+    ],
+  ];
 }
 
-# Solr configuration override
-if (!empty(getenv('SOLR_HOST')) && !empty(getenv('SOLR_PORT'))) {
-    $config['search_api.server.solr']['backend_config']['connector_config']['host'] = getenv('SOLR_HOST');
-    $config['search_api.server.solr']['backend_config']['connector_config']['port'] = getenv('SOLR_PORT') ?: 8983;
-    $config['search_api.server.solr']['backend_config']['connector_config']['core'] = getenv('SOLR_CORE') ?: 'dev';
+if ($platformsh->inRuntime()) {
+  // Configure private and temporary file paths.
+  if (!isset($settings['file_private_path'])) {
+    $settings['file_private_path'] = $platformsh->appDir . '/private';
+  }
+  if (!isset($settings['file_temp_path'])) {
+    $settings['file_temp_path'] = $platformsh->appDir . '/tmp';
+  }
+
+// Configure the default PhpStorage and Twig template cache directories.
+  if (!isset($settings['php_storage']['default'])) {
+    $settings['php_storage']['default']['directory'] = $settings['file_private_path'];
+  }
+  if (!isset($settings['php_storage']['twig'])) {
+    $settings['php_storage']['twig']['directory'] = $settings['file_private_path'];
+  }
+
+  // Set the project-specific entropy value, used for generating one-time
+  // keys and such.
+  $settings['hash_salt'] = empty($settings['hash_salt']) ? $platformsh->projectEntropy : $settings['hash_salt'];
+
+  // Set the deployment identifier, which is used by some Drupal cache systems.
+  $settings['deployment_identifier'] = $settings['deployment_identifier'] ?? $platformsh->treeId;
 }
 
-// Hash salt
-$settings['hash_salt'] = getenv('HASH_SALT');
+// The 'trusted_hosts_pattern' setting allows an admin to restrict the Host header values
+// that are considered trusted.  If an attacker sends a request with a custom-crafted Host
+// header then it can be an injection vector, depending on how the Host header is used.
+// However, Platform.sh already replaces the Host header with the route that was used to reach
+// Platform.sh, so it is guaranteed to be safe.  The following line explicitly allows all
+// Host headers, as the only possible Host header is already guaranteed safe.
+$settings['trusted_host_patterns'] = ['.*'];
+
+// Import variables prefixed with 'drupalsettings:' into $settings
+// and 'drupalconfig:' into $config.
+foreach ($platformsh->variables() as $name => $value) {
+  $parts = explode(':', $name);
+  list($prefix, $key) = array_pad($parts, 3, null);
+  switch ($prefix) {
+    // Variables that begin with `drupalsettings` or `drupal` get mapped
+    // to the $settings array verbatim, even if the value is an array.
+    // For example, a variable named drupalsettings:example-setting' with
+    // value 'foo' becomes $settings['example-setting'] = 'foo';
+    case 'drupalsettings':
+    case 'drupal':
+      $settings[$key] = $value;
+      break;
+    // Variables that begin with `drupalconfig` get mapped to the $config
+    // array.  Deeply nested variable names, with colon delimiters,
+    // get mapped to deeply nested array elements. Array values
+    // get added to the end just like a scalar. Variables without
+    // both a config object name and property are skipped.
+    // Example: Variable `drupalconfig:conf_file:prop` with value `foo` becomes
+    // $config['conf_file']['prop'] = 'foo';
+    // Example: Variable `drupalconfig:conf_file:prop:subprop` with value `foo` becomes
+    // $config['conf_file']['prop']['subprop'] = 'foo';
+    // Example: Variable `drupalconfig:conf_file:prop:subprop` with value ['foo' => 'bar'] becomes
+    // $config['conf_file']['prop']['subprop']['foo'] = 'bar';
+    // Example: Variable `drupalconfig:prop` is ignored.
+    case 'drupalconfig':
+      if (count($parts) > 2) {
+        $temp = &$config[$key];
+        foreach (array_slice($parts, 2) as $n) {
+          $prev = &$temp;
+          $temp = &$temp[$n];
+        }
+        $prev[$n] = $value;
+      }
+      break;
+  }
+}
+
