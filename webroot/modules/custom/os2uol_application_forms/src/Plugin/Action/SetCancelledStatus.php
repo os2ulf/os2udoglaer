ec0b46757576f0f6b59a4583b1bf924e17352365
Søren Due Bonde <sdb@novicell.dk>
2024-08-30 15:05:55 +0200

    OS2UOL-693: Add custom actions to change status on free course request nodes

---
+++ webroot/modules/custom/os2uol_application_forms/src/Plugin/Action/SetCancelledStatus.php
@@ -0,0 +1,33 @@
+<?php
+namespace Drupal\os2uol_application_forms\Plugin\Action;
+
+use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\node\Entity\Node;
+
+/**
+ * @Action(
+ *   id = "set_cancelled_status",
+ *   label = @Translation("Set status to Cancelled"),
+ *   type = "node"
+ * )
+ */
+class SetCancelledStatus extends ActionBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute($entity = NULL) {
+    if ($entity instanceof Node) {
+      $entity->set('field_rfc_status', 'cancelled');
+      $entity->save();
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+    return $object->access('update', $account, $return_as_object);
+  }
+}
---
+++ webroot/modules/custom/os2uol_application_forms/src/Plugin/Action/SetRefusedStatus.php
@@ -0,0 +1,33 @@
+<?php
+namespace Drupal\os2uol_application_forms\Plugin\Action;
+
+use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\node\Entity\Node;
+
+/**
+ * @Action(
+ *   id = "set_refused_status",
+ *   label = @Translation("Set status to Refused"),
+ *   type = "node"
+ * )
+ */
+class SetRefusedStatus extends ActionBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute($entity = NULL) {
+    if ($entity instanceof Node) {
+      $entity->set('field_rfc_status', 'refused');
+      $entity->save();
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+    return $object->access('update', $account, $return_as_object);
+  }
+}
