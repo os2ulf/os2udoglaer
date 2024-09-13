508e8b86f11ca5557b275212b250ca3f02ebbd9d
Mads Nørgaard <madsnorgaard@live.dk>
2024-09-13 09:53:53 +0200

    OS2UOL-764 HTML email signatur config per domain (#347)
    * OS2UOL-764 HTML email signatur config per domain
    
    * OS2UOL-764 make full html
    
    * OS2UOL-764 make basic_html

--- webroot/modules/custom/os2uol_settings/src/Form/DomainSettingsForm.php
+++ webroot/modules/custom/os2uol_settings/src/Form/DomainSettingsForm.php
@@ -109,6 +109,14 @@ final class DomainSettingsForm extends ConfigFormBase {
       '#tags' => FALSE,
     ];
 
+    $form['email_signature'] = [
+      '#type' => 'text_format',
+      '#title' => $this->t('Email signature'),
+      '#default_value' => $config->get('email_signature')['value'] ?? '',
+      '#format' => $config->get('email_signature')['format'] ?? 'basic_html',
+      '#description' => $this->t('Enter the HTML for the email signature. This will be used as a token in email sendouts.'),
+    ];
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -136,6 +144,7 @@ final class DomainSettingsForm extends ConfigFormBase {
       ->set('text_negative_color', $form_state->getValue('text_negative_color'))
       ->set('site_tracking_script', $form_state->getValue('site_tracking_script'))
       ->set('free_course_application_reference', $form_state->getValue('free_course_application_reference'))
+      ->set('email_signature', $form_state->getValue('email_signature'))
       ->save();
 
     parent::submitForm($form, $form_state);
