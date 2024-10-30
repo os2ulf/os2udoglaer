6691e72357fb17257de398b76f05460acda30905
Mads Nørgaard <madsnorgaard@live.dk>
2024-10-30 18:13:32 +0100

    Release/2024 12 (#421)
    * OS2UOL-156: Added facet for post number and city to internship search
    
    * OS2UOL-156: Added facet for company to internship search
    
    * OS2UOL-156: Added facet for guarantee parners to internship search
    
    * OS2UOL-156: Fixed BooleanEmptyOption returning only one option
    
    * OS2UOL-875 created separate block displays for the roles (#409)
    
    * OS2UOL-97 som minor fixes and introducing automatic unpublish functionality (#410)
    
    * Minor issues fix
    
    * OS2UOL-697: Adjusted anonymization script to delete all revisions
    
    * OS2UOL-697: Added update script to remove revisions from already anonymized nodes
    
    * OS2UOL-883: Added form validation ff "Søg tilskud til transport" or "Søg om refusion af forløbet" is checked, then "Hvem kan få tilskud" is required
    
    * OS2UOL-834: Adjusted ddev providers
    
    * OS2UOL-834: Added and configured domain_login_restrict module
    
    * OS2UOL-834: Adjusted entity presave function on user entity to not add current domain to user if current domain is default domain
    
    * OS2UOL-834: Added domain_login_restrict patch to be able to export restrict settings to config
    
    * OS2UOL-697: Fixed up anonymization script
    
    * OS2UOL-881 update to the automated unpublish functionality (#418)
    
    * OS2UOL-875 created separate block displays for the roles (#420)
    
    ---------
    
    Co-authored-by: Erik Petra <epe@novicell.dk>
    Co-authored-by: Søren Due Bonde <sdb@novicell.dk>
    Co-authored-by: Søren Bonde <soren@duebonde.dk>

--- webroot/modules/custom/os2uol_moderation/src/Form/PurgeSettingsForm.php
+++ webroot/modules/custom/os2uol_moderation/src/Form/PurgeSettingsForm.php
@@ -32,10 +32,25 @@ class PurgeSettingsForm extends ConfigFormBase {
 
     $form['days'] = [
       '#type' => 'number',
-      '#title' => $this->t('Days'),
-      '#description' => $this->t('Number of days after which nodes in the "trash" state should be purged.'),
+      '#title' => $this->t('Purge Days'),
+      '#description' => $this->t('Days after which nodes in the trash should be purged.'),
       '#default_value' => $config->get('days') ?? 30,
       '#min' => 1,
+    ];
+    $form['first_warning'] = [
+      '#type' => 'number',
+      '#title' => $this->t('First Warning Interval (days)'),
+      '#default_value' => $config->get('first_warning') ?? 14,
+    ];
+    $form['second_warning'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Second Warning Interval (days)'),
+      '#default_value' => $config->get('second_warning') ?? 7,
+    ];
+    $form['unpublish_interval'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Unpublish Interval (days)'),
+      '#default_value' => $config->get('unpublish_interval') ?? 430,
     ];
 
     return parent::buildForm($form, $form_state) + $form;
@@ -47,9 +62,11 @@ class PurgeSettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('os2uol_moderation.settings')
       ->set('days', $form_state->getValue('days'))
+      ->set('first_warning', $form_state->getValue('first_warning'))
+      ->set('second_warning', $form_state->getValue('second_warning'))
+      ->set('unpublish_interval', $form_state->getValue('unpublish_interval'))
       ->save();
 
     parent::submitForm($form, $form_state);
   }
-
 }
