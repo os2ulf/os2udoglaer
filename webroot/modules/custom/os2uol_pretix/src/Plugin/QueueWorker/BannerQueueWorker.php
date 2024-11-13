aecaeb0c79851eb3ae574bcc05bbf9243acaf235
Mads Nørgaard <madsnorgaard@live.dk>
2024-11-13 14:07:57 +0100

    OS2UOL-942 fixup cron failing due to error in BannerQueueWorker

--- webroot/modules/custom/os2uol_pretix/src/Plugin/QueueWorker/BannerQueueWorker.php
+++ webroot/modules/custom/os2uol_pretix/src/Plugin/QueueWorker/BannerQueueWorker.php
@@ -15,12 +15,48 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class BannerQueueWorker extends QueueWorkerBase implements ContainerFactoryPluginInterface {
 
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    // TODO: Implement create() method.
+  /**
+   * The logger service.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
+   * Constructs a new BannerQueueWorker object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   The logger service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, $logger) {
+      parent::__construct($configuration, $plugin_id, $plugin_definition);
+      $this->logger = $logger;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+        $configuration,
+        $plugin_id,
+        $plugin_definition,
+        $container->get('logger.channel.default')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function processItem($data) {
-    // TODO: Implement processItem() method.
+      // Process the item in the queue
+      $this->logger->info('Processing data: @data', ['@data' => json_encode($data)]);
   }
 
 }
