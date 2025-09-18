a529791aba64f5d4f064473f31199c72c56e6a9d
Erik Petra <epe@novicell.dk>
2025-09-18 11:38:31 +0200

    UOLOS2-479: Added missing fields to menu link item transform

--- webroot/modules/custom/os2uol_menus/src/Plugin/Transform/Block/CustomMenuTransformBlock.php
+++ webroot/modules/custom/os2uol_menus/src/Plugin/Transform/Block/CustomMenuTransformBlock.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\os2uol_menus\Plugin\Transform\Block;
 
+use Drupal\menu_link_content\Entity\MenuLinkContent;
 use Drupal\transform_api\Plugin\Transform\Block\SystemMenuTransformBlock;
 use Drupal\transform_api\Transform\EntityTransform;
 
@@ -26,13 +27,22 @@ class CustomMenuTransformBlock extends SystemMenuTransformBlock {
    * @return array
    *   The JSON array.
    */
-  protected function transformMenuItems(array $items) {
+  protected function transformMenuItems(array $items): array {
     $result = [];
 
     foreach ($items as $array) {
+      /** @var MenuLinkContent $entity */
       $entity = $array['original_link']->getEntity();
 
       $item = (new EntityTransform($entity))->transform();
+
+      $item['label'] = $array['title'];
+      $item['title'] = $array['title'];
+      $item['enabled'] = $entity->isEnabled();
+      $item['expanded'] = $entity->isExpanded();
+      $item['link']['url'] = $array['url']->toString();
+      $item['field_icon'] = $this->getIconUrl($entity);
+      $item['field_description'] = $entity->get('field_description')->value;
 
       $item['below'] = $this->transformMenuItems($array['below']);
 
@@ -40,6 +50,17 @@ class CustomMenuTransformBlock extends SystemMenuTransformBlock {
     }
 
     return $result;
+  }
+
+  protected function getIconUrl(MenuLinkContent $item): ?string {
+    if (!$item->hasField('field_icon') || $item->get('field_icon')->isEmpty()) {
+      return NULL;
+    }
+
+    /** @var \Drupal\file\Entity\File $file */
+    $file = $item->get('field_icon')->entity;
+
+    return $file?->createFileUrl(FALSE);
   }
 
 }
