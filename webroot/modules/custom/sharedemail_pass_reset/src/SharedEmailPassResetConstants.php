009547063d3e56fabc555e7ef1fb602aa3aec0c4
Erik Petra <epe@novicell.dk>
2026-03-23 14:30:23 +0100

    UOLOS2-712: Enabled shared email

---
+++ webroot/modules/custom/sharedemail_pass_reset/src/SharedEmailPassResetConstants.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\sharedemail_pass_reset;
+
+/**
+ * Class SharedEmailPassResetConstants.
+ *
+ * @package Drupal\sharedemail_pass_reset
+ */
+abstract class SharedEmailPassResetConstants {
+
+  public const STRATEGY_CONDITIONAL = 'conditional';
+  public const STRATEGY_USERNAME = 'username';
+  public const STRATEGY_ALL = 'all';
+
+}
